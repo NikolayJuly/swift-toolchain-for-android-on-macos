dfbe39f48a03cbc14ce8270b82addf61c2beefa2
Nikolay Dzhulay <nikolay.july@gmail.com>
2023-03-19 18:20:24 +0000

    Minor fix in dureation string formating

--- Sources/SwiftBuilder/Utils/TimeMesurement.swift
+++ Sources/SwiftBuilder/Utils/TimeMesurement.swift
@@ -12,13 +12,13 @@ final class TimeMesurement {
         switch duration {
         case 60..<60*60:
             let seconds = Int(duration)
-            return String(format: "%02dm%02ds", seconds/60, seconds%60)
+            return String(format: "%dm%02ds", seconds/60, seconds%60)
         case 60.0*60.0..<24*60*60:
             let intSeconds = Int(duration)
             let hours = intSeconds/(60*60)
             let minutes = (intSeconds % (60*60))/60
             let seconds = intSeconds % 60
-            return String(format: "%02dh%02dm%02ds", hours, minutes, seconds)
+            return String(format: "%dh%02dm%02ds", hours, minutes, seconds)
         default:
             return String(format: "%.1fs", duration)
         }
